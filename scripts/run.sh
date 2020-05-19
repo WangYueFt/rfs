4548e5acecffe72be8ba0cdf7ab7dc3d2577d6d6
Yonglong Tian <yonglong@mit.edu>
2020-05-19 17:11:55 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@
 python train_supervised.py --trial pretrain --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
 
 # distillation
+# setting '-a 1.0' should give simimlar performance
 python train_distillation.py -r 0.5 -a 0.5 --path_t /path/to/teacher.pth --trial born1 --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
 
 # evaluation
