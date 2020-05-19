4468eb1c1f9066c60b0ac494de1bbf73de76e25f
Yonglong Tian <yonglong@mit.edu>
2020-05-19 17:08:04 -0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@
 python train_supervised.py --trial pretrain --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
 
 # distillation
-python train_distillation.py -r 0.5 -a 1.0 --path_t /path/to/teacher.pth --trial born1 --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
+python train_distillation.py -r 0.5 -a 0.5 --path_t /path/to/teacher.pth --trial born1 --model_path /path/to/save --tb_path /path/to/tensorboard --data_root /path/to/data_root
 
 # evaluation
 python eval_fewshot.py --model_path /path/to/student.pth --data_root /path/to/data_root
